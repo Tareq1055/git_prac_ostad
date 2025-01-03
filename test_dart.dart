a57e36f1d7d0c870ad38aca025e5cbb2d811f61e
Mohammed Tareq <tareqnavy@yahoo.com>
2025-01-03 19:59:43 +0600

    first

--- test_dart.dart
+++ test_dart.dart
@@ -1,3 +1,11 @@
 main(){
 
 }
+
+echo "# git_prac_ostad" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Tareq1055/git_prac_ostad.git
+git push -u origin main
